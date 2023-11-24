008191b4c30302b93e217db25cda78745f0fd139
caldardn <caldardn@gmail.com>
2023-11-24 15:48:22 -0500

    adding user routes

--- models/user.js
+++ models/user.js
@@ -9,4 +9,47 @@ class User extends Model {
     }
 }
 
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull:false,
+        unique: true,
+        validate: {
+            len: [8]
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [8]
+        }
+    }
+},
+{
+    hooks: {
+        // set up beforeCreate lifecycle "hook" functionality
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData
+        },
+        // set up beforeUpdate lifecycle "hook" functionality
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData
+        }
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+})
+
 module.exports = User;
