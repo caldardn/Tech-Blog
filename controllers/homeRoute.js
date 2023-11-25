183eaa922f061c61b54ba7653b9c3a5b1837c9cd
caldardn <caldardn@gmail.com>
2023-11-25 15:38:33 -0500

    adding updated code

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -23,7 +23,7 @@ router.get("/post/:id", async (req, res) => {
     const postData = await Post.findByPk(req.params.id, {
       include: [
         { model: User, attributes: ["username"] }, 
-        { model: Comment, include: [User]}
+        { model: Comment, attributes: ["username"]}
     ],
     });
 
@@ -38,6 +38,25 @@ router.get("/post/:id", async (req, res) => {
   }
 });
 
+router.get("/dashboard", async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+            include: [{ model: User, attributes: ["username"] }],
+        });
+        const posts = postData.map((post) => post.get({ plain: true }));
+        res.render("dashboard", {
+            posts,
+            logged_in: req.session.logged_in,
+        });
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
 router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
@@ -46,6 +65,14 @@ router.get("/login", (req, res) => {
   }
 
   res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/dashboard");
+    return;
+  }
+    res.render("signup");
 });
 
 module.exports = router;
